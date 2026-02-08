3d83cdef4e7aa6b42675d70e0fa8a2e63581c5c5
pedro <pedromenendez93@gmail.com>
2026-02-08 10:57:52 +0100

    refactor: Rename MyApplicationRunner to BookamaniaApplication and update startup message

--- src/main/java/com/bookamania/BookamaniaApplication.java
+++ src/main/java/com/bookamania/BookamaniaApplication.java
@@ -6,14 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyApplicationRunner implements ApplicationRunner {
+public class BookamaniaApplication implements ApplicationRunner {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyApplicationRunner.class, args);
+        SpringApplication.run(BookamaniaApplication.class, args);
     }
 
     @Override
     public void run(ApplicationArguments args) {
-        System.out.println("Spring boot java archetype example");
+        System.out.println("Bookamania is ready to cook!");
     }
 }
